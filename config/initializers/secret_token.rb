26a3f437e84bb339d6f56ecc2c639b238c331c4d
Seth <seth.johnson@desiringgod.org>
2011-04-18 22:55:17 -0500

    Heroku?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlashCards::Application.config.secret_token = '3911362d0f791592f4e7481888e8b6379b584b8b17d2eac1156e6d32fd9781c960307c7d2c73e19452981e4c4c0e9ee27664d8df85c00bcb5c1ac7c064a33a6c'
